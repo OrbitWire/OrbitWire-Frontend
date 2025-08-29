b2a07a8400386c2f487775b2ee26fccedff9d10e
unknown <dattatreya.chakraborty12@gmail.com>
2025-08-29 09:32:41 +0530

    responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -130,121 +130,129 @@ const Navbar = () => {
                 </NavigationMenu>
             </div>
 
-            <div className="md:hidden">
-                <Sheet open={open} onOpenChange={setOpen}>
-                    <SheetTrigger>
-                        <Menu size={23} className={`${open && "hidden"}`} />
-                    </SheetTrigger>
-                    <SheetContent side="right" className="p-3 sm:p-6 w-[50%]">
-                        <SheetHeader>
-                            <SheetTitle
-                                className={
-                                    "text-lg sm:text-xl sm:[letter-spacing:0.15rem]"
-                                }
-                            >
-                                Navigation Menu
-                            </SheetTitle>
-                        </SheetHeader>
+            <div className="flex items-center justify-center">
+                {/* theme toggle button */}
+                <div className="mr-5">
+                    <ThemeToggleButton variant="circle" start="top-right" />
+                </div>
+                <div className="md:hidden flex items-center justify-center">
+                    <Sheet open={open} onOpenChange={setOpen}>
+                        <SheetTrigger>
+                            <Menu
+                                size={23}
+                                className={`mr-3 ${open && "hidden"}`}
+                            />
+                        </SheetTrigger>
+                        <SheetContent
+                            side="right"
+                            className="p-3 sm:p-6 w-[50%]"
+                        >
+                            <SheetHeader>
+                                <SheetTitle
+                                    className={
+                                        "text-lg sm:text-xl sm:[letter-spacing:0.15rem]"
+                                    }
+                                >
+                                    Navigation Menu
+                                </SheetTitle>
+                            </SheetHeader>
 
-                        {/* Mobile Navigation */}
+                            {/* Mobile Navigation */}
 
-                        <div className="flex flex-col space-y-2">
-                            <Link
-                                href="/"
-                                className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
-                                    pathname === "/"
-                                        ? "bg-primary text-primary-foreground"
-                                        : "text-parimary-foreground hover:bg-accent/30"
-                                }`}
-                                onClick={handleLinkClick}
-                            >
-                                <House
-                                    size={20}
-                                    strokeWidth={2}
-                                    className="inline"
-                                />
-                                <Separator
-                                    orientation="vertical"
-                                    className={"bg-secondary/50 mr-1"}
-                                />
-                                Home
-                            </Link>
+                            <div className="flex flex-col space-y-2">
+                                <Link
+                                    href="/"
+                                    className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
+                                        pathname === "/"
+                                            ? "bg-primary text-primary-foreground"
+                                            : "text-parimary-foreground hover:bg-accent/30"
+                                    }`}
+                                    onClick={handleLinkClick}
+                                >
+                                    <House
+                                        size={20}
+                                        strokeWidth={2}
+                                        className="inline"
+                                    />
+                                    <Separator
+                                        orientation="vertical"
+                                        className={"bg-secondary/50 mr-1"}
+                                    />
+                                    Home
+                                </Link>
 
-                            <Link
-                                href="/gaming"
-                                className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
-                                    pathname === "/gaming"
-                                        ? "bg-primary text-primary-foreground"
-                                        : "text-parimary-foreground hover:bg-accent/30"
-                                }`}
-                                onClick={handleLinkClick}
-                            >
-                                <Gamepad2
-                                    strokeWidth={2}
-                                    size={20}
-                                    className="inline"
-                                />
-                                <Separator
-                                    orientation="vertical"
-                                    className={"bg-secondary/50 mr-1"}
-                                />
-                                Gaming
-                            </Link>
-                            <Link
-                                href="/esports"
-                                className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1  space-x-1 text-sm sm:text-[16px] ${
-                                    pathname === "/esports"
-                                        ? "bg-primary text-primary-foreground"
-                                        : "text-parimary-foreground hover:bg-accent/30"
-                                }`}
-                                onClick={handleLinkClick}
-                            >
-                                <LiaTrophySolid className="inline text-xl" />
-                                <Separator
-                                    orientation="vertical"
-                                    className={"bg-secondary/50 mr-1"}
-                                />
-                                E-Sports
-                            </Link>
-                            <Link
-                                href="/sports"
-                                className={`px-3 py-2 rounded-md flex border border-border/50 items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
-                                    pathname === "/sports"
-                                        ? "bg-primary text-primary-foreground"
-                                        : "text-parimary-foreground hover:bg-accent/30"
-                                }`}
-                                onClick={handleLinkClick}
-                            >
-                                <MdOutlineSportsTennis className="inline text-xl" />
-                                <Separator
-                                    orientation="vertical"
-                                    className={"bg-secondary/50 mr-1"}
-                                />
-                                Sports
-                            </Link>
-                            <Link
-                                href="/technology"
-                                className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
-                                    pathname === "/technology"
-                                        ? "bg-primary text-primary-foreground"
-                                        : "text-parimary-foreground hover:bg-accent/30"
-                                }`}
-                                onClick={handleLinkClick}
-                            >
-                                <GrTechnology className="inline text-xl " />
-                                <Separator
-                                    orientation="vertical"
-                                    className={"bg-secondary/50 mr-1"}
-                                />
-                                Technology
-                            </Link>
-                        </div>
-                    </SheetContent>
-                </Sheet>
-            </div>
-
-            <div className="mr-10">
-                <ThemeToggleButton variant="circle" start="top-right" />
+                                <Link
+                                    href="/gaming"
+                                    className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
+                                        pathname === "/gaming"
+                                            ? "bg-primary text-primary-foreground"
+                                            : "text-parimary-foreground hover:bg-accent/30"
+                                    }`}
+                                    onClick={handleLinkClick}
+                                >
+                                    <Gamepad2
+                                        strokeWidth={2}
+                                        size={20}
+                                        className="inline"
+                                    />
+                                    <Separator
+                                        orientation="vertical"
+                                        className={"bg-secondary/50 mr-1"}
+                                    />
+                                    Gaming
+                                </Link>
+                                <Link
+                                    href="/esports"
+                                    className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1  space-x-1 text-sm sm:text-[16px] ${
+                                        pathname === "/esports"
+                                            ? "bg-primary text-primary-foreground"
+                                            : "text-parimary-foreground hover:bg-accent/30"
+                                    }`}
+                                    onClick={handleLinkClick}
+                                >
+                                    <LiaTrophySolid className="inline text-xl" />
+                                    <Separator
+                                        orientation="vertical"
+                                        className={"bg-secondary/50 mr-1"}
+                                    />
+                                    E-Sports
+                                </Link>
+                                <Link
+                                    href="/sports"
+                                    className={`px-3 py-2 rounded-md flex border border-border/50 items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
+                                        pathname === "/sports"
+                                            ? "bg-primary text-primary-foreground"
+                                            : "text-parimary-foreground hover:bg-accent/30"
+                                    }`}
+                                    onClick={handleLinkClick}
+                                >
+                                    <MdOutlineSportsTennis className="inline text-xl" />
+                                    <Separator
+                                        orientation="vertical"
+                                        className={"bg-secondary/50 mr-1"}
+                                    />
+                                    Sports
+                                </Link>
+                                <Link
+                                    href="/technology"
+                                    className={`px-3 py-2 rounded-md border border-border/50 flex items-center gap-1 space-x-1 text-sm sm:text-[16px] ${
+                                        pathname === "/technology"
+                                            ? "bg-primary text-primary-foreground"
+                                            : "text-parimary-foreground hover:bg-accent/30"
+                                    }`}
+                                    onClick={handleLinkClick}
+                                >
+                                    <GrTechnology className="inline text-xl " />
+                                    <Separator
+                                        orientation="vertical"
+                                        className={"bg-secondary/50 mr-1"}
+                                    />
+                                    Technology
+                                </Link>
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </div>
             </div>
         </nav>
     );
